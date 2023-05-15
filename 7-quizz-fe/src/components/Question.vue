ffce4b13d2ddc38b2eecc533e19ab8c96d6c847e
A_Hameau <official.hameau@gmail.com>
2023-05-15 11:51:19 +0200

    front init

---
+++ 7-quizz-fe/src/components/Question.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-card>
+        <v-card-title>
+        <h3>{{ `Question #${number}` }}</h3>
+        </v-card-title>
+        <v-card-text>
+        <p>{{ question.body }}</p>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'QuestionComponent',
+
+  props: {
+    number: {
+      type: Number,
+      required: true,
+    },
+    question: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    hey: 'Hello World',
+  }),
+};
+</script>
+
+<style></style>
