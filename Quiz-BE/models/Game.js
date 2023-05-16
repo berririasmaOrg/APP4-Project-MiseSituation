ab19cabbf13e0bdc2a1f16c2b9360b5ec6c8b337
Adesh CANNOU <adeshca156@gmail.com>
2023-05-16 17:44:54 +0200

    API rankings + create game

--- Quiz-BE/models/Game.js
+++ Quiz-BE/models/Game.js
@@ -2,11 +2,20 @@ const { model, Schema } = require("mongoose");
 
 const gameSchema = Schema(
   {
-    score: { type: Number, required: true },
-    time: { type: Number, required: true }
+    score: { type: Number },
+    time: { type: Number },
+    quiz: {
+      type: Schema.Types.ObjectId,
+      ref: "Quiz",
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   { timestamps: true }
 );
-
 
 module.exports = model("Game", gameSchema);
